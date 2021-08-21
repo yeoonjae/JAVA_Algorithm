aaa51eae086c992a7a0f3ac4971f64de1025418b
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2021-08-21 11:24:14 +0900

    큰 수 더하기 풀이

---
+++ src/BigSum.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger big = new BigInteger(sc.next());
+        BigInteger big2 = new BigInteger(sc.next());
+
+        BigInteger result = big.add(big2);
+        System.out.println(result);
+    }
+
+}
