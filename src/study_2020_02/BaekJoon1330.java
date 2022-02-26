19fee05170b33246bdf4221af383dbc4c1620b0d
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2022-02-26 23:41:06 +0900

    알고리즘 문제풀이

---
+++ src/study_2020_02/BaekJoon1330.java
@@ -0,0 +1,19 @@
+package study_2020_02;
+
+import java.util.Scanner;
+
+public class BaekJoon1330 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    sc.close();
+
+    System.out.println((a > b) ? ">" : (a < b) ? "<" : "=");
+
+
+  }
+
+}
---
+++ src/study_2020_02/BaekJoon1924.java
@@ -0,0 +1,28 @@
+package study_2020_02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon1924 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    int[] monthArray = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    String[] dateArray = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+    int month = Integer.parseInt(st.nextToken());
+    int day = Integer.parseInt(st.nextToken());
+
+    int dayCount = 0;
+
+    for (int i = 0; i < month - 1; i++) {
+      dayCount += monthArray[i];
+    }
+    dayCount += day;
+
+    System.out.println(dateArray[dayCount % 7]);
+  }
+}
